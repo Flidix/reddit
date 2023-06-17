e8e0ca3eeaad600b406431c178adbc340b51389d
Flidix <marko.lazarchuk@gmail.com>
2023-06-17 14:54:01 +0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-      MongooseModule.forRoot('mongodb+srv://ADMIN:ADMIN@cluster0.e3t0bgf.mongodb.net/?retryWrites=true&w=majority'),
+      MongooseModule.forRoot('mongodb+srv://ADMIN:password@cluster0.e3t0bgf.mongodb.net/?retryWrites=true&w=majority'),
       forwardRef(() => TrendModule),
       UserModule,
       AuthModule,
